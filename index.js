c2ce9c22045d159896679ae5796f828529f73f73
Henry Tian <tyrneh@gmail.com>
2024-07-02 22:42:26 -0600

    refactoring into an express app

---
+++ index.js
@@ -0,0 +1,29 @@
+// install packages
+const express = require('express');
+const app = express();
+const favicon = require('serve-favicon');
+const ejsMate = require('ejs-mate');
+
+// setting up views directory
+const path = require('path');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// serving static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set the view engine to ejs
+app.engine('ejs', ejsMate);
+
+// favicon
+app.use(favicon(__dirname + '/public/favicon/favicon.png'));
+
+//--------------- ROUTES -------------------- //
+app.get('/', (req, res) => {
+  res.render('home.ejs');
+});
+
+// start server
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
